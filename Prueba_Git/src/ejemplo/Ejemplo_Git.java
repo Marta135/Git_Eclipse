d3dda6a2bbff138e5638ce22baf6147b6a02cc95
Marta <martitaoun@gmail.com>
2020-05-21 17:28:56 +0200

    Versión inicial del programa

---
+++ Prueba_Git/src/ejemplo/Ejemplo_Git.java
@@ -0,0 +1,26 @@
+/*Programa Java para leer las longitudes de los lados de un triangulo*/
+
+package ejemplo;
+
+import java.util.Scanner;
+
+public class Ejemplo_Git {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Introduzca la longitud del primer lado:");
+		float lado1 = teclado.nextFloat();
+		
+		System.out.println("Introduzca la longitud del segundo lado:");
+		float lado2 = teclado.nextFloat();
+		
+		System.out.println("Introduzca la longitud del tercer lado:");
+		float lado3 = teclado.nextFloat();
+
+	}
+
+}
+
+
